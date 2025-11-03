7b05ab6f654f8e5a0e8bce19b98d362710ff11b7
Exe16Kishan <kishanshukla53@gmail.com>
2025-11-03 22:56:50 +0530

    feat( lats,long): add latitude and longitude

--- apps/web/src/app/ride/page.tsx
+++ apps/web/src/app/ride/page.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React ,{useEffect, useState} from 'react'
 
 function page() {
+  const [location, setlocation] = useState({lat:0,long:0})
+  useEffect (()=>{
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos)=>{
+        setlocation({lat :pos.coords.latitude ,long : pos.coords.longitude})
+      },null , {enableHighAccuracy:true,timeout:3000})
+    }
+  },[])
+
+  const handleRide = ()=>{
+    // send coords to api
+  }
   return (
-    <div>page</div>
+    <div>
+      {/* dummy api check */}
+      <button>Click to request ride</button>
+    </div>
   )
 }
 
